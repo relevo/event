7aec14aa967c3288a779e046b9a98d39bb3a0617
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-04-30 16:54:58 +0200

    Add comment

--- src/RoutesEvents.php
+++ src/RoutesEvents.php
@@ -5,5 +5,10 @@ namespace Relevo\Event;
 
 interface RoutesEvents
 {
+    /**
+     * Route events to listeners.
+     *
+     * @param EventRouter $router
+     */
     public function routeEvents(EventRouter $router);
 }
